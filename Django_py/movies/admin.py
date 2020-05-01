08e92b8bd993e9ea80860b6d9d966e03177ed430
Karlos Gevorgyan <karloss.jan@gmail.com>
2020-05-01 13:52:58 +0400

    My project

---
+++ Django_py/movies/admin.py
@@ -0,0 +1,143 @@
+from django.contrib import admin
+
+from django.utils.safestring import mark_safe
+
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категория"""
+    list_display = ("id", "name")
+    list_display_links = ("name",)
+
+
+class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} widht="100" height="110"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+    inlines = [MovieShotsInline, ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    actions = ["publish", "unpublish"]
+    readonly_fields = ("get_image",)
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", ("poster", "get_image"))
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} widht="150" height="160"')
+
+    def unpublish(self,request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            massage_bit = "1 запись была обновлена"
+        else:
+            massage_bit = f"{row_update} записей были обновлены"
+        self.massage_user(request, f"{massage_bit}")
+
+    def publish(self,request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            massage_bit = "1 запись была обновлена"
+        else:
+            massage_bit = f"{row_update} записей были обновлены"
+        self.massage_user(request, f"{massage_bit}")
+
+    publish.short_description = "Опубликовать"
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = "Снять с публикации"
+    unpublish.allowed_permissions = ('change', )
+
+    get_image.short_description = "Постер"
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent_id", "movie_id", "id")
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("name", "id")
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("name", "age", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} widht="90" height="100"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ("star", "movie", "ip")
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ("title", "movie")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} widht="90" height="100"')
+
+    get_image.short_description = "Изображение"
+
+
+admin.site.register(RatingStar)
+
+admin.site.site_title = "MOVIES"
+admin.site.site_header = "HOLLYWOOD"
